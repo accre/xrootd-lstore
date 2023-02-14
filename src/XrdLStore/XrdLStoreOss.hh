cb01ca1760572793ace3f4ee931ecb537dc1e7a1
Eric Appelt <eric.appelt@gmail.com>
2023-02-14 13:02:24 -0600

    Add XrdLStoreOss main header file

---
+++ src/XrdLStore/XrdLStoreOss.hh
@@ -0,0 +1,61 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2023 by Vanderbilt University
+//------------------------------------------------------------------------------
+// This file is part of a third-party plugin to the XRootD software suite.
+//
+// XRootD is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// XRootD is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with XRootD.  If not, see <http://www.gnu.org/licenses/>.
+//------------------------------------------------------------------------------
+
+#ifndef __LSTORE_OSS_HH__
+#define __LSTORE_OSS_HH__
+
+#include <string>
+#include <XrdOss/XrdOss.hh>
+
+//------------------------------------------------------------------------------
+//! This class implements XrdOss interface for usage with an LStore filesystem.
+//! It should be loaded via the ofs.osslib directive.
+//!
+//! TODO: Explain how to connect to a given LStore server through configuration
+//!
+//! Note that the definition of a default via the ofs.osslib directive may
+//! clash with one used in a ofs.xattrlib directive. In case both directives
+//! have a default and they are different, the behavior is not defined.
+//! In case one of the two only has a default, it will be applied for both plugins.
+//------------------------------------------------------------------------------
+
+class XrdLStoreOss : public XrdOss {
+public:
+  XrdLStoreOss(const char *, XrdSysError &);
+  virtual ~XrdLstoreOss();
+
+  int Configure(const char *, XrdSysError &);
+
+  virtual int     Chmod(const char *, mode_t mode, XrdOucEnv *eP=0);
+  virtual int     Create(const char *, const char *, mode_t, XrdOucEnv &, int opts=0);
+  virtual int     Init(XrdSysLogger *, const char*);
+  virtual int     Mkdir(const char *, mode_t mode, int mkpath=0, XrdOucEnv *eP=0);
+  virtual int     Remdir(const char *, int Opts=0, XrdOucEnv *eP=0);
+  virtual int     Rename(const char *, const char *, XrdOucEnv *eP1=0, XrdOucEnv *eP2=0);
+  virtual int     Stat(const char *, struct stat *, int opts=0, XrdOucEnv *eP=0);
+  virtual int     StatFS(const char *path, char *buff, int &blen, XrdOucEnv *eP=0);
+  virtual int     StatVS(XrdOssVSInfo *sP, const char *sname=0, int updt=0);
+  virtual int     Truncate(const char *, unsigned long long, XrdOucEnv *eP=0);
+  virtual int     Unlink(const char *path, int Opts=0, XrdOucEnv *eP=0);
+  virtual XrdOssDF *newDir(const char *tident);
+  virtual XrdOssDF *newFile(const char *tident);
+
+};
+
+#endif /* __LSTORE_OSS_HH__ */
