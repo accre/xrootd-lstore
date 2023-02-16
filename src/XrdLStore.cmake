3be0aada8cca3b66ae04aa307926b5e63126e268
Eric Appelt <eric.appelt@gmail.com>
2023-02-15 19:52:00 -0600

    First try at XrdLStore cmake file

---
+++ src/XrdLStore.cmake
@@ -0,0 +1,52 @@
+include_directories( ${XROOTD_INCLUDE_DIR} )
+include_directories( ${LSTORE_INCLUDE_DIR} )
+include_directories( ${CMAKE_SOURCE_DIR}/src )
+
+#-------------------------------------------------------------------------------
+# The XrdLStore module
+#-------------------------------------------------------------------------------
+set( LIB_XRD_LSTORE XrdLStore-${PLUGIN_VERSION} )
+
+add_library(
+  ${LIB_XRD_LSTORE}
+  MODULE
+  XrdLStore/XrdLStoreOss.cc       XrdLStore/XrdLStoreOss.hh )
+
+target_link_libraries(
+  ${LIB_XRD_LSTORE}
+  ${XROOTD_LIBRARIES}
+  ${LSTORE_LIBS} )
+
+set_target_properties(
+  ${LIB_XRD_LSTORE}
+  PROPERTIES
+  INTERFACE_LINK_LIBRARIES ""
+  LINK_INTERFACE_LIBRARIES "" )
+
+#-------------------------------------------------------------------------------
+# The XrdLStoreXattr module
+#-------------------------------------------------------------------------------
+set( LIB_XRD_LSTORE_XATTR XrdLStoreXattr-${PLUGIN_VERSION} )
+
+add_library(
+  ${LIB_XRD_LSTORE_XATTR}
+  MODULE
+  XrdCeph/XrdLStoreXAttr.cc   XrdCeph/XrdLStoreXAttr.hh )
+
+target_link_libraries(
+  ${LIB_XRD_LSTORE_XATTR}
+  ${XROOTD_LIBRARIES}  )
+
+set_target_properties(
+  ${LIB_XRD_LSTORE_XATTR}
+  PROPERTIES
+  INTERFACE_LINK_LIBRARIES ""
+  LINK_INTERFACE_LIBRARIES "" )
+
+#-------------------------------------------------------------------------------
+# Install
+#-------------------------------------------------------------------------------
+install(
+  TARGETS ${LIB_XRD_LSTORE} ${LIB_XRD_LSTORE_XATTR}
+  LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR} )
+
